61de1594522c8d0af22c7e695bc4a3179be31a85
Pilag6 <pilag6@gmail.com>
2023-12-06 15:46:29 +0100

    Add images to Readme

--- test/checkFunctions.js
+++ test/checkFunctions.js
@@ -24,7 +24,7 @@ function checkGuess(randomNumber, userGuess) {
 }
 
 const randomNumExample = 7021;
-const userNumExample = 7142;
+const userNumExample = 8142;
 
 const result = checkGuess(randomNumExample, userNumExample);
 console.log(result); // { wellLocated: 1, misplaced: 3 }
@@ -33,50 +33,50 @@ console.log("-".repeat(20));
 // --------------------------------------------------
 // FUNCTION TO GENERATE A RANDOM 4-DIGIT DIFFERENT NUMBER
 
-function generateRandomNumber() {
-    let randomNumber = Math.floor(Math.random() * 10000);
+// function generateRandomNumber() {
+//     let randomNumber = Math.floor(Math.random() * 10000);
 
-    while (randomNumber < 1000 || hasRepeatedDigits(randomNumber)) {
-        randomNumber = Math.floor(Math.random() * 10000);
-    }
+//     while (randomNumber < 1000 || hasRepeatedDigits(randomNumber)) {
+//         randomNumber = Math.floor(Math.random() * 10000);
+//     }
 
-    return randomNumber;
-}
+//     return randomNumber;
+// }
 
-const randomNum = generateRandomNumber();
-console.log(randomNum);
-console.log("-".repeat(20));
+// const randomNum = generateRandomNumber();
+// console.log(randomNum);
+// console.log("-".repeat(20));
 
-// --------------------------------------------------
-// FUNCTION TO CHECK IF THE NUMBER HAS REPEATED DIGITS
+// // --------------------------------------------------
+// // FUNCTION TO CHECK IF THE NUMBER HAS REPEATED DIGITS
 
-function hasRepeatedDigits(number) {
-    const digits = String(number).split("");
+// function hasRepeatedDigits(number) {
+//     const digits = String(number).split("");
 
-    const uniqueDigits = digits.filter((digit, index) => {
-        return digits.indexOf(digit) === index;
-    });
+//     const uniqueDigits = digits.filter((digit, index) => {
+//         return digits.indexOf(digit) === index;
+//     });
 
-    return digits.length !== uniqueDigits.length;
-}
+//     return digits.length !== uniqueDigits.length;
+// }
 
-const hasRepeated = hasRepeatedDigits(1234);
-const hasRepeatedAgain = hasRepeatedDigits(1224);
+// const hasRepeated = hasRepeatedDigits(1234);
+// const hasRepeatedAgain = hasRepeatedDigits(1224);
 
-console.log(hasRepeated); // false
-console.log(hasRepeatedAgain); // true
+// console.log(hasRepeated); // false
+// console.log(hasRepeatedAgain); // true
 
-// --------------------------------------------------
-// FUNCTION TO DISPLAY GAME STATUS
-function displayStatus(userGuess, wellLocated, misplaced, guessesLeft) {
-    console.log(`
-    1. Your guess: ${userGuess}
+// // --------------------------------------------------
+// // FUNCTION TO DISPLAY GAME STATUS
+// function displayStatus(userGuess, wellLocated, misplaced, guessesLeft) {
+//     console.log(`
+//     1. Your guess: ${userGuess}
         
-    2. Well Located: ${wellLocated} | Misplaced: ${misplaced}
+//     2. Well Located: ${wellLocated} | Misplaced: ${misplaced}
         
-    3. Guesses left: ${guessesLeft}
-  `);
-}
+//     3. Guesses left: ${guessesLeft}
+//   `);
+// }
 
-displayStatus(1234, 1, 2, 3);
-console.log("-".repeat(20));
+// displayStatus(1234, 1, 2, 3);
+// console.log("-".repeat(20));
